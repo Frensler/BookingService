b2afcd74f8cb38d464c004c156ea395747800cc1
Frensler <91434328+Frensler@users.noreply.github.com>
2023-01-10 23:42:57 +0100

    Detailed view button now enable

--- BookingService/myapp/urls.py
+++ BookingService/myapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('rooms/<int:roomId>', views.roomDetails_index, name='roomDetails'),
     path('rooms/addNewRoom', views.addNewRoom_index, name='addNewRoom'),
     path('rooms/<int:roomId>/checkOut', views.checkOut, name='checkOut'),
+    path('rooms/<int:roomId>/checkIn', views.checkIn, name='checkIn'),
     path('reservations/', views.reservations_index, name='reservations'),
     path('clients/', views.clients_index, name='clients'),
     path('clients/edit/<int:clientId>', views.addOrEditClient_index, name='editClient'),
